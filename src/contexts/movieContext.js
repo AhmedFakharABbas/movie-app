8723646420dde113fb7d4040b31bab835f47f7f4
AhmedFakharABbas <ahmedfakharabbas@gmail.com>
2025-03-18 12:49:38 +0500

    updated movie context

--- src/contexts/movieContext.js
+++ src/contexts/movieContext.js
@@ -1,1 +1,33 @@
 import { createContext, useState, useContext, useEffect } from "react";
+import Favourites from "../pages/favorites";
+const MovieContext = createContext();
+export const useMovieContext = () => useContext();
+export const MovieProvider = ({ children }) => {
+  const favKey = "favorites";
+  const [favorites, setFavorites] = useState([]);
+  useEffect(() => {
+    const storedFavs = localStorage.getItem(favKey);
+    if (storedFavs) setFavorites(JSON.parse(storedFavs));
+  }, []);
+  useEffect(() => {
+    localStorage.setItem(favKey, JSON.stringify(favorites));
+  }, [favorites]);
+  const addToFavorites = (movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
+  const removeFromFavorites = (movieId) => {
+    setFavorites((prev) => prev.filter((movie) => movie.id !== movieId));
+  };
+  const isFavorite = (movieId) => {
+    return Favourites.some((movie) => movie.id === movieId);
+  };
+  const values = {
+    addToFavorites,
+    removeFromFavorites,
+    isFavorite,
+  };
+  return (
+    <MovieContext.Provider value={values}>{children}</MovieContext.Provider>
+  );
+  
+};
